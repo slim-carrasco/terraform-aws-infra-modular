767bf7250ff46b9d419cc0e34d38f5966ca11a5d
Slim Carrasco <e.slim.carrasco@gmail.com>
2026-09-26 04:02:48 +0000

    chore/cambio a use_lockfile

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "frizka-terraform-state"
     key            = "environment/dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tfstate"
+    use_lockfile   = true
     encrypt        = true
   }
 }
